edc30dc2b05d5d5e10639a7aac8e1a4f389120b2
Gareth Coles <gdude2002@gmail.com>
2015-02-19 13:12:02 +0000

    Plugin base

---
+++ src/main/java/Plugin.java
@@ -0,0 +1,14 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
