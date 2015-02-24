a1f4c8509a0451ec26c8a8b38d1c25dacac650e0
Gareth <gdude2002@gmail.com>
2015-02-24 14:01:40 +0000

    Remove unused import

--- src/main/java/com/archivesmc/archblock/Plugin.java
+++ src/main/java/com/archivesmc/archblock/Plugin.java
@@ -9,7 +9,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
 
 import java.util.List;
 
