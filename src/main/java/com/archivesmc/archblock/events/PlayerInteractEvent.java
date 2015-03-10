353709d649eb80bb3f0357ee381571c59051de36
Gareth Coles <gdude2002@gmail.com>
2015-03-10 08:49:09 +0000

    Null-checking

--- src/main/java/com/archivesmc/archblock/events/PlayerInteractEvent.java
+++ src/main/java/com/archivesmc/archblock/events/PlayerInteractEvent.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.UUID;
 
@@ -24,6 +25,12 @@ public class PlayerInteractEvent implements Listener {
         Action action = event.getAction();
 
         if (block == null) {
+            return;
+        }
+
+        ItemStack itemStack = event.getPlayer().getItemInHand();
+
+        if (itemStack == null) {
             return;
         }
 
